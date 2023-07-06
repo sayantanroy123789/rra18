3f1c7f7812704750a4b3bec41d0506b6cc3a8a7f
sayantan <sayantan.roy123789@gmail.com>
2023-07-07 02:33:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [time,setTime]= useState(0);
+  useEffect(()=>{
+    
+    
+  return()=> clearInterval(id.current);
+
+  }, []);
+  let id= useRef();
+  function handleTime(){
+   id.current= setInterval(()=>{
+    setTime((prev)=>prev+1);
+    console.log(time);
+  }, 1000);
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Timer {time}</h1>
+      <button onClick={() => handleTime()}>start</button>
+      <button onClick={() => clearInterval(id.current)}>pause</button>
+      <button onClick={() =>{
+         clearInterval(id.current)
+         setTime(0);
+         }}>reset</button>
     </div>
   );
 }
